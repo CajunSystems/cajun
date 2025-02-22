c85ff78e51a30680dc684c359a8191d9c9f8e59b
Pradeep Samuel <pradeep.samuel90@gmail.com>
2025-02-22 20:45:45 +0530

    Added docs, added capability to send messages to self for actor and refactored actor id to be consistent

--- lib/src/main/java/systems/cajun/Actor.java
+++ lib/src/main/java/systems/cajun/Actor.java
@@ -1,5 +1,6 @@
 package systems.cajun;
 
+import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.StructuredTaskScope;
@@ -8,9 +9,22 @@ public abstract class Actor<Message> {
 
     private final BlockingQueue<Message> mailbox;
     private volatile boolean isRunning;
+    private final String actorId;
+    private final ActorSystem system;
+    private final Pid pid;
 
-    public Actor() {
+    public Actor(ActorSystem system) {
+        this.system = system;
+        this.actorId = UUID.randomUUID().toString();
         this.mailbox = new LinkedBlockingQueue<>();
+        this.pid = new Pid(actorId, system);
+    }
+
+    public Actor(ActorSystem system, String actorId) {
+        this.system = system;
+        this.actorId = actorId;
+        this.mailbox = new LinkedBlockingQueue<>();
+        this.pid = new Pid(actorId, system);
     }
 
     protected abstract void receive(Message message);
@@ -28,6 +42,14 @@ public abstract class Actor<Message> {
                 Thread.currentThread().interrupt();
             }
         });
+    }
+
+    public Pid self() {
+        return pid;
+    }
+
+    public String getActorId() {
+        return actorId;
     }
 
     public void tell(Message message) {
@@ -50,10 +72,16 @@ public abstract class Actor<Message> {
         }
     }
 
+    public boolean isRunning() {
+        return isRunning;
+    }
+
     public void stop() {
         if (!isRunning) {
             return;
         }
         isRunning = false;
+        // Intimate system just in case it was actor who triggered stop
+        system.shutdown(actorId);
     }
 }
